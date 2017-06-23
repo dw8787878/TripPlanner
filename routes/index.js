051355731726ef7eb4179e1690048bfe65c477d2
Darryn Wu <darryn8wu@gmail.com>
2017-06-23 11:47:36 -0400

    basic index complete

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,20 @@
 const route = require("express").Router();
+const db = require("../models");
 
 route.get("/", (req, res) => {
-  res.render("index.html");
+
+  var promHotel = db.Hotel.findAll({});
+  var promActiv = db.Activity.findAll({});
+  var promRest = db.Restaurant.findAll({});
+
+  Promise.all([promHotel, promActiv, promRest])
+    .then(function (values) {
+      res.render("index.html", {
+      Hotel: values[0],
+      Activity: values[1],
+      Restaurant: values[2]
+    });
+  }).catch(console.error)
 });
 
 route.post("/login", (req, res) => {
